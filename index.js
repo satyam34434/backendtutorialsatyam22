076af5150e317463026196c142f2d15415ed33aa
Satyam Gupta <satyam34434@gmail.com>
2024-08-18 19:46:37 +0530

    Updated Port

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-
+require('dotenv').config()
 const app = express()
 
-const port = 3000
+const port = process.env.PORT
 
 app.get('/',(req,res) => {
     res.send('Working')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
     console.log(`App running at port ${port}`)
 })

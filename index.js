798b6487c3e3d0b8671def0595f5254439c8414e
Satyam Gupta <satyam34434@gmail.com>
2024-08-18 19:47:23 +0530

    Again Updated Port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.get('/',(req,res) => {
     res.send('Working')
